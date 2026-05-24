7774042ccb34bec87652c471f993bba1b9c390b9
Dhakshitha <dhakshithasivakumar28@gmail.com>
2026-05-24 12:54:08 +0530

    Implemented warehouses API

--- app/api/warehouses/route.ts
+++ app/api/warehouses/route.ts
@@ -1,7 +1,24 @@
 import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
 
 export async function GET() {
-  return NextResponse.json({
-    message: "Warehouses API working",
-  });
+
+  try {
+
+    const warehouses =
+      await prisma.warehouse.findMany();
+
+    return NextResponse.json(warehouses);
+
+  } catch (error) {
+
+    return NextResponse.json(
+      {
+        error: "Failed to fetch warehouses",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
